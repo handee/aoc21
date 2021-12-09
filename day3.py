73ace99c1f7c99b84bbb67b281eaa0a967d16e58
tayberry <hmd1@aber.ac.uk>
2021-12-09 19:50:39 +0000

    updated d3

--- day3.py
+++ day3.py
@@ -1,13 +1,24 @@
 import numpy as np 
 
-fn='input/day3test.txt'
-#fn='input/input3.1.txt'
+#fn='input/test3.txt'
+fn='input/day3.txt'
 
 def bools2dec(boolarray):
+   
     rv=boolarray.astype(int)
     t=2 ** np.flip(np.arange(len(rv)))
     return(np.sum(np.multiply(rv,t)))
-        
+    
+
+def string2dec(binstring):
+    decimal=0
+    for char in binstring:
+        if char=='1':
+            decimal=decimal*2+1
+        else:
+            decimal=decimal*2
+    return(decimal)
+            
 
 def part_one():
     with open(fn) as fp:
@@ -34,33 +45,60 @@ def part_one():
     print(f"{g} {e} power {g*e}")
 
 
-
-def part_two():
-    with open(fn) as fp:
-        ds=[line.strip() for line in fp]
-    n = len(ds[0])
-    sums1=np.zeros(n)
-    sums0=np.zeros(n)
-    for d in ds:
+def workoutmostpopular(candidates,n):
+    sums1=[0]*n
+    sums0=[0]*n
+    for d in candidates:
         for i,c in enumerate(d):
             if c=='1':
                 sums1[i]+=1
             else: # it must be 0
                 sums0[i]+=1
-    gamma=np.zeros(n,dtype=np.bool)
+    gamma=[0]*n
     for i,b in enumerate(sums1):
-        if b>sums0[i]:
+        if b>=sums0[i]:
             gamma[i]=1
-    epsilon=np.invert(gamma)
+    return(gamma)
+  
+def part_two():
+    with open(fn) as fp:
+        ds=[line.strip() for line in fp]
+    n = len(ds[0])
 
     candidates=ds
+    candidates2=ds.copy()
     element=0
 
-    while(len(candidates)>1):
-        b=filter(lambda x: x[element]==gamma[element],candidates)
+    while(len(candidates2)>1):
+        
+        gamma=workoutmostpopular(candidates2,n)
+        epsilon=[1-i for i in gamma]
+        b=[]
+        for i in range(len(candidates2)):
+            if int(candidates2[i][element])==epsilon[element]:
+                b.append(candidates2[i])
+
+        
         element+=1
-        candidates=list(b)
-        print(candidates)
+        candidates2=b
+    
+
+    element=0
+    while(len(candidates)>1):
+        
+        gamma=workoutmostpopular(candidates,n)
+        b=[]
+        for i in range(len(candidates)):
+            if int(candidates[i][element])==gamma[element]:
+                b.append(candidates[i])
+        
+        element+=1
+        candidates=b
+    print(f"Gamma candidate = {candidates[0]}")
+    print(f"Epsilon candidate = {candidates2[0]}")
+    a=string2dec(candidates[0])
+    b=string2dec(candidates2[0])
+    print(f"oxygen={a} co2={b} ans={a*b}")
 
 
 part_two()
