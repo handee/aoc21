3010a43306e395a7f1d1eb75864475bbdef38530
tayberry <hmd1@aber.ac.uk>
2021-12-16 09:05:36 +0000

    forgot about the possibility of backtracking - fixed now

--- day15.py
+++ day15.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #fn='input/test15.txt'
 fn='input/day15.txt'
@@ -27,6 +28,51 @@ def calc_cost(a):
         for i in range(1,a.shape[0]):
             costs[j,i]=min(costs[j-1,i],costs[j,i-1])+a[j,i]
     return(costs)
+
+def point_inside_array(p,a):
+    rv=True
+    if p[0] < 0 or p[1] < 0 or p[0] >= a.shape[0] or p[1] >= a.shape[1]:
+        rv=False
+    return rv
+
+def calc_cost_4w(a):
+    # array to hold points we've seen before
+    pts=[]
+    logical_max=a.shape[0]*a.shape[1]*9
+    costs=np.ones_like(a)*logical_max
+    costs[0,0]=0
+    done=np.zeros_like(costs) #array to work out which we've done
+    done[0,:]+=1
+    done[:,0]+=1
+    pts.append([0,1])
+    pts.append([1,0]) 
+    while len(pts)>0:
+        #cp = current_point
+        cp=pts[0]
+        # get neighbours
+        pn=[[cp[0]-1,cp[1]],[cp[0],cp[1]-1],[cp[0]+1,cp[1]],[cp[0],cp[1]+1]]
+        # prune neighbour list if they're outside of the array
+        # also look up the cost of valid ones whilst we're looping
+        min_cost_neighbour=logical_max
+        vpn=[]
+        while len(pn)>0:
+            p=pn.pop()
+            if point_inside_array(p,a):
+                if costs[p[0],p[1]]<min_cost_neighbour:
+                    min_cost_neighbour=costs[p[0],p[1]]
+                vpn.append(p)
+        #the best cost of the current point so far is...
+        done[cp[0],cp[1]]+=1
+        costs[cp[0],cp[1]]=min(costs[cp[0],cp[1]],min_cost_neighbour+a[cp[0],cp[1]])
+        # then add each valid neighbour to our point search list
+        for p in vpn:
+            if (done[p[0],p[1]]<4):
+                if p not in pts: # if it's not on our list already add it
+                    pts.append(p)
+        pts.pop(0)
+    return(costs)
+            
+
    
 def scale_cave(a,s):
     w=a.shape[0]
@@ -47,11 +93,26 @@ def partone():
     costs=calc_cost(caves)
     print(costs[-1,-1])
 
+
+
+def save_visualisation(manual):
+    framen='output/cave.png'
+    scale=2
+    manual_large= np.zeros(np.array(manual.shape) * scale)
+
+    for j in range(manual.shape[0]):
+        for k in range(manual.shape[1]):
+            manual_large[j * scale: (j+1) * scale, k * scale: (k+1) * scale] = manual[j, k]
+    plt.imsave(framen, manual_large, cmap='inferno')
+
+
+
 def parttwo():
     caves=readinfile(fn)
     newc=scale_cave(caves,5)
+    save_visualisation(newc)
     print(newc[0,:])
-    costs=calc_cost(newc)
+    costs=calc_cost_4w(newc)
     print(costs[-1,-1])
     print(costs[0,:])
     print(costs[-1,:])
